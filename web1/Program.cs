58e6e68736768e0c7d98688d27c13e887418a86d
zmrbak <20995658@qq.com>
2025-04-25 14:43:04 +0800

    只保留端口

--- web1/Program.cs
+++ web1/Program.cs
@@ -18,18 +18,19 @@ namespace web1
                 var serverAddresses = context.RequestServices.GetRequiredService<IServer>().Features
                     .Get<IServerAddressesFeature>()?.Addresses;
                 var serverPorts = serverAddresses != null ? $"Server Listening Ports: {string.Join(", ", serverAddresses)}\n" : "";
+                return $"{serverPorts}\n";
 
 
-                var url = $"The URL is: {context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}\n";
-                var headers = new System.Text.StringBuilder("Request Headers:\n");
+                //var url = $"The URL is: {context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}\n";
+                //var headers = new System.Text.StringBuilder("Request Headers:\n");
 
-                // 遍历所有请求头并格式化输出
-                foreach (var header in context.Request.Headers)
-                {
-                    headers.AppendLine($"{header.Key}: {header.Value}");
-                }
+                //// 遍历所有请求头并格式化输出
+                //foreach (var header in context.Request.Headers)
+                //{
+                //    headers.AppendLine($"{header.Key}: {header.Value}");
+                //}
 
-                return $"{serverPorts}{url}\n{headers}";
+                //return $"{serverPorts}{url}\n{headers}";
             });
             app.Run();
         }
