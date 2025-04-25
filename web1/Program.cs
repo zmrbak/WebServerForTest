2d53858d096b8e2cbc3427e4228a0dea80a484d1
zmrbak <20995658@qq.com>
2025-04-25 14:36:22 +0800

    添加显示监听端口

--- web1/Program.cs
+++ web1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Hosting.Server;
+
 namespace web1
 {
     public class Program
@@ -11,8 +14,22 @@ namespace web1
             app.UseAuthorization();
             app.MapGet("/{*path}", (HttpContext context) =>
             {
-                var url = $"The url is :{context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}\n";
-                return url;
+                // 获取服务器实际监听的地址和端口
+                var serverAddresses = context.RequestServices.GetRequiredService<IServer>().Features
+                    .Get<IServerAddressesFeature>()?.Addresses;
+                var serverPorts = serverAddresses != null ? $"Server Listening Ports: {string.Join(", ", serverAddresses)}\n" : "";
+
+
+                var url = $"The URL is: {context.Request.Scheme}://{context.Request.Host}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}\n";
+                var headers = new System.Text.StringBuilder("Request Headers:\n");
+
+                // 遍历所有请求头并格式化输出
+                foreach (var header in context.Request.Headers)
+                {
+                    headers.AppendLine($"{header.Key}: {header.Value}");
+                }
+
+                return $"{serverPorts}{url}\n{headers}";
             });
             app.Run();
         }
